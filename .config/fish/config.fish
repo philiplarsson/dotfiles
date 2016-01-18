aa78cf9754879049d9b71f45d4ac028b79a75c09
Philip Larsson <elt11pla@student.lu.se>
2016-01-18 16:58:19 +0100

    Set default editor to vim in fish shell

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,22 +3,6 @@ set -g -x PATH /usr/local/bin $PATH
 # Add 256 color to tmux when using gnome-terminal
 export TERM="xterm-256color"
 
-
-    # Add this to your ~/.config/fish/config.fish
-    # Syntax:
-    # To just rerun your last command, simply type '!!'
-    # This forked version supports "sudo !!" via two functions.
-#function !!
-#        eval $history[1] $argv
-#end function
-
-#function sudo
-#    if test $argv
-#        if test $argv = "!!"
-#           eval /usr/bin/sudo $history[1]
-#        else
-#           eval /usr/bin/sudo $argv
-#        end
-#    end
-#end
+# make Vim the default editor
+set --export EDITOR "vim -f"
 
