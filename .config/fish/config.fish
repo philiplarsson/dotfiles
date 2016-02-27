0b0ca1056180d0bb5fc3c02ac07b51f7b8e3bce4
Philip Larsson <elt11pla@student.lu.se>
2016-02-27 17:58:29 +0100

    Add 2 plugins and config for fish and tmux

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,9 @@
 set -g -x PATH /usr/local/bin $PATH
 
 # Add 256 color to tmux when using gnome-terminal
-export TERM="xterm-256color"
+# export TERM="xterm-256color"
+export TERM="screen-256color"
+# export TERM=xterm-256color
 
 # make Vim the default editor
 set --export EDITOR "vim -f"
