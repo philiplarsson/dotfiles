dbc8286feb7df64d08f1b3366fd671d0e257a100
Philip Larsson <elt11pla@student.lu.se>
2017-03-08 17:36:28 +0100

    Update emacs configs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -25,6 +25,7 @@
 ;; Yasnippet
 (require 'yasnippet)
 (yas-global-mode t)
+
 ;;; ---------- Keybindings ----------
 (global-set-key (kbd "M-h") 'backward-char)
 (global-set-key (kbd "M-n") 'forward-char)
@@ -32,12 +33,28 @@
 (global-set-key (kbd "M-t") 'next-line)
 
 (global-set-key (kbd "C-c C-SPC") 'ace-jump-mode)
+
+;; Move between windows using M-o
+(global-set-key (kbd "M-o") 'other-window)
+
+;; Evil Keybindings
+;; Use C-e to go to end of line
+(define-key evil-normal-state-map (kbd "C-e") 'evil-end-of-line)
+; Make movement keys work like they should
+(define-key evil-normal-state-map (kbd "<remap> <evil-next-line>") 'evil-next-visual-line)
+(define-key evil-normal-state-map (kbd "<remap> <evil-previous-line>") 'evil-previous-visual-line)
+(define-key evil-motion-state-map (kbd "<remap> <evil-next-line>") 'evil-next-visual-line)
+(define-key evil-motion-state-map (kbd "<remap> <evil-previous-line>") 'evil-previous-visual-line)
+
 ;;; ---------- General Settings ----------
 ;; Remove toolbar
 (when (window-system)
   (tool-bar-mode -1))
 ;; Remove Menu Bar
 (menu-bar-mode -1)
+;; Remove Scrollbar
+(scroll-bar-mode -1)
+
 ;; Wrap text on words
 (global-visual-line-mode t)
 
@@ -54,6 +71,18 @@
 ;; Allow y and n instead of yes and no
 (fset 'yes-or-no-p 'y-or-n-p)
 
+;; Use IDO Mode
+(ido-mode 1)
+(setq ido-enable-flex-matching t)
+;; IDO File matching (C-x C-f) (find files) with IDO
+(setq ido-eveywhere t)
+
+;; Auto Indent When Pressing enter
+(define-key global-map (kbd "RET") 'newline-and-indent)
+
+;; windmove Package to move to window using shift+arrow keys
+(windmove-default-keybindings)
+
 ;; ---------- Mouse ----------
 ;;; scroll one line at a time (less "jumpy" than defaults) 
 (setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
@@ -61,6 +90,7 @@
 (setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
 (setq scroll-step 1) ;; keyboard scroll one line at a time
 
+;; ---------- Variables set by Custom ----------
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -68,8 +98,9 @@
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
  '(custom-safe-themes
-   (quote
-    ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" default)))
+	 (quote
+		("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" default)))
+ '(line-move-visual t)
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
