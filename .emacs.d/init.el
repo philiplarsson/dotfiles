fdcf6301b957762fbd5643aa03f343ef52f32cd6
Philip Larsson <elt11pla@student.lu.se>
2019-05-06 22:43:59 +0200

    Update Emacs configuration

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -45,11 +45,11 @@
      ("XXXX" . "#dc752f")
      ("???" . "#dc752f"))))
  '(ns-use-native-fullscreen nil)
- '(package-selected-packages (quote (which-key use-package)))
+ '(package-selected-packages (quote (htmlize ace-window which-key use-package)))
  '(pdf-view-midnight-colors (quote ("#655370" . "#fbf8ef"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
