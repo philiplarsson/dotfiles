db3f70e6ed2ec37074452f7fb7155ca6873ee70b
Philip Larsson <philip.larsson.1991@gmail.com>
2020-03-19 13:13:35 +0100

    Emacs Bankruptcy
    I declared emacs bankruptcy. This review replaces the config with a new
    one.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,24 +1,11 @@
-(require 'package)
 (setq package-enable-at-startup nil)
-(setq package-archives
-      '(("gnu"     . "https://elpa.gnu.org/packages/")
-        ("melpa-stable" . "https://stable.melpa.org/packages/")
-        ("melpa"        . "https://melpa.org/packages/"))
-      package-archive-priorities
-      '(("melpa-stable" . 10)
-        ("gnu"     . 5)
-        ("melpa"        . 0)))
+(setq package-archives '(("gnu" . "http://mirrors.163.com/elpa/gnu/")
+                         ("melpa" . "https://melpa.org/packages/")
+                         ("org" . "http://orgmode.org/elpa/")))
 
 (package-initialize)
-
-; make sure that use-package is installed
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-; Load configuration from org-babel config file
 (require 'org)
-(org-babel-load-file "~/.emacs.d/myconfig.org")
+(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -27,43 +14,14 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#454545" "#d65946" "#6aaf50" "#baba36" "#598bc1" "#ab75c3" "#68a5e9" "#bdbdb3"])
- '(column-number-mode t)
- '(current-language-environment "UTF-8")
+ '(custom-enabled-themes '(doom-one-light))
  '(custom-safe-themes
-   (quote
-    ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "23f1d50c7687a59aabf9773e40413e97e5c29b3b22c065f7c866cfcfc4538343" default)))
- '(hl-todo-keyword-faces
-   (quote
-    (("TODO" . "#dc752f")
-     ("NEXT" . "#dc752f")
-     ("THEM" . "#2d9574")
-     ("PROG" . "#3a81c3")
-     ("OKAY" . "#3a81c3")
-     ("DONT" . "#f2241f")
-     ("FAIL" . "#f2241f")
-     ("DONE" . "#42ae2c")
-     ("NOTE" . "#b1951d")
-     ("KLUDGE" . "#b1951d")
-     ("HACK" . "#b1951d")
-     ("TEMP" . "#b1951d")
-     ("FIXME" . "#dc752f")
-     ("XXX" . "#dc752f")
-     ("XXXX" . "#dc752f")
-     ("???" . "#dc752f"))))
- '(lsp-ui-doc-use-webkit t)
- '(lsp-ui-sideline-enable nil)
- '(ns-use-native-fullscreen nil)
- '(package-selected-packages
-   (quote
-    (ag helm-ag helm-projectile helm flycheck-pos-tip powerline flycheck-color-mode-line js2-mode flycheck company company-mode highlight-symbol highlight-simbyl diff-hl clean-aindent-mode duplicate-thing undo-tree volatile-highlights exec-path-from-shell markdown-mode htmlize ace-window which-key use-package)))
- '(pdf-view-midnight-colors (quote ("#655370" . "#fbf8ef")))
- '(show-paren-mode t)
+   '("e1ecb0536abec692b5a5e845067d75273fe36f24d01210bf0aa5842f2a7e029f" "1ed5c8b7478d505a358f578c00b58b430dde379b856fbcb60ed8d345fc95594e" "d5f8099d98174116cba9912fe2a0c3196a7cd405d12fa6b9375c55fc510988b5" "e074be1c799b509f52870ee596a5977b519f6d269455b84ed998666cf6fc802a" "1d50bd38eed63d8de5fcfce37c4bb2f660a02d3dff9cbfd807a309db671ff1af" "cb96a06ed8f47b07c014e8637bd0fd0e6c555364171504680ac41930cfe5e11e" "fa3bdd59ea708164e7821574822ab82a3c51e262d419df941f26d64d015c90ee" "99ea831ca79a916f1bd789de366b639d09811501e8c092c85b2cb7d697777f93" "615123f602c56139c8170c153208406bf467804785007cdc11ba73d18c3a248b" "dde8c620311ea241c0b490af8e6f570fdd3b941d7bc209e55cd87884eb733b0e" "7f791f743870983b9bb90c8285e1e0ba1bf1ea6e9c9a02c60335899ba20f3c94" default))
+ '(package-selected-packages '(crux avy which-key doom-themes))
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "Roboto Mono")))))
